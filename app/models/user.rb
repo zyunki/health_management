606adca8c2844d07aa2d34b9b64591c7d608f735
zyunki <iwaju2160@gmail.com>
2024-08-12 11:33:27 +0900

    ユーザー新規登録機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,34 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :password_confirmation, presence: true
+  validate :passwords_match
+
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+
+  validates :gender, presence: true
+
+  validates :introduction, presence: true
+
+  validates :position, presence: true
+
+  private
+
+
+  def passwords_match
+    if password != password_confirmation
+      errors.add(:password_confirmation, "とパスワードが一致しません")
+    end
+  end
 end
+
